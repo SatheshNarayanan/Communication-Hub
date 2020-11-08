be42c38df794c139be361efca606387eac29ceb1
Sathesh Narayanan <64146715+SatheshNarayanan@users.noreply.github.com>
2020-11-08 22:48:22 +0530

    Update Job.js

--- src/schedule/Job.js
+++ src/schedule/Job.js
@@ -1,11 +1,11 @@
-const cron = require("node-cron")
-const sendMessage = require("../communication/sendSms")
+// const cron = require("node-cron")
+// const sendMessage = require("../communication/sendSms")
 
-//schedule task that runs every minute
-const task = cron.schedule("*/1 * * * *",async () => {
-    await sendMessage("+918300040510","Hi there","Testing scheduled data")
-},{
-    scheduled : true
-})
+// //schedule task that runs every minute
+// const task = cron.schedule("*/1 * * * *",async () => {
+//     await sendMessage("+918300040510","Hi there","Testing scheduled data")
+// },{
+//     scheduled : true
+// })
 
-module.exports = task
+// module.exports = task
